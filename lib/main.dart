3c72ed2f8ce81a9b158c6aa9aa1cc9071ccb78cf
MargoM <margaret.michenko@gmail.com>
2024-07-01 10:05:07 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('Margo is genius'),
-));
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.brown)
+      home: Text('BloomQuit', style: TextStyle(
+        fontSize: 24,
+        color: Colors.amber,
+        fontFamily: 'Arial'
+        
+      ))
+    );
+  }
+}
