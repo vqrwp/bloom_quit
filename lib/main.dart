11302289b5b9ec3aa47440dba2431e5ce12b56b6
MargoM <margaret.michenko@gmail.com>
2024-07-21 18:34:35 +0300

    added font, text

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
  
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.brown)
-      home: Text('BloomQuit', style: TextStyle(
-        fontSize: 24,
-        color: Colors.amber,
-        fontFamily: 'Arial'
-        
-      ))
+      theme: ThemeData(primaryColor: Color.fromARGB(255, 63, 172, 152)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BloomQuit'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('BloomQuit', style: TextStyle(
+            fontSize: 50,
+            color: Color.fromARGB(255, 40, 120, 120),
+            fontFamily: 'WorkforceCombine'
+          ),)
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Press'),
+          backgroundColor: Color.fromARGB(255, 60, 154, 80), 
+          onPressed: () {
+            print('Pressed');
+            },
+        ),
+      ),
     );
   }
 }
